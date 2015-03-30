d86f056996d3fd06edbd74817a7d8e2651c30022
abcd <819284657@qq.com>
2015-03-30 18:01:10 +0800

    代码优化

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,9 @@
 document.getElementById("img").onclick = function() {
-	if (document.getElementById("em").className == "ema") {
-		document.getElementById("em").className = "em";
+	if (document.getElementById("em").className.indexOf("ema ") >-1) {
+		document.getElementById("em").className = document.getElementById("em").className.replace('ema ','em ');
 		document.getElementById("card").className = "card";
 	} else {
-		document.getElementById("em").className = "ema";
+		document.getElementById("em").className = document.getElementById("em").className.replace('em ','ema ');
 		document.getElementById("card").className = "cardh";
 	}
 }
